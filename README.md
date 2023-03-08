0310963d55859ba3594437940ad01dc8da5b6125
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2023-03-07 23:09:42 -0300

    postando sobre o exercício de JS

---
+++ README.md
@@ -0,0 +1,8 @@
+# EXERCICIO DO CURSO DE JS
+Este exercício é para treinar lógica de programação!
+
+## OBJETIVO
+Descobrir o último digito de qualquer CPF(não precisa ser literalmente o último, pode ser antepenúltimo e etc.)
+
+### TECNOLODIA USADA
+* JavaScript
---
+++ script.js
@@ -0,0 +1,38 @@
+// 705.484.450-52
+/*
+
+7x  0x  5x  4x  8x  4x  4x  5x  0x
+10  9   8   7   6   5   4   3   2 // quanto maior a quantidade de numeros acima, aumentará em baxo tbm
+70  0   40  28  48  20  16  15  0 = 237
+
+11 - (237 % 11) = 5 (primeiros digitos)
+se o número digitado for maior que 9, consideraremos 0
+assim monmtaremos parte por parte o cpf, como um quebra cabeça
+
+7x  0x  5x  4x  8x  4x  4x  5x  0x  5x
+11  10  9   8   7   6   5   4   3   2
+77  0   45  32  56  24  20  20  0   10 = 284
+11 - (284 % 11) = 2 (primeiros digitos)
+*/
+
+// 070.987.720-03 -> o ultimo numero tem que ser 3, pois corresponde ao ultimo numero do cpf
+const cpf = "070.987.720-0";
+const cpfLimpo = cpf.replace(/\D+/g, "");
+let cpfArray = Array.from(cpfLimpo);
+let count = cpfLimpo.length + 2;
+
+const cpfTratado = cpfArray
+  .reduce((acumulator, value) => {
+    count--;
+    let resulte = count * Number(value);
+    acumulator.push(resulte);
+    return acumulator;
+  }, [])
+  .join("+");
+
+const cpfDigit = () => {
+  let restOfNumberOfCPF = 11 - (eval(cpfTratado) % 11);
+  return restOfNumberOfCPF > 9 ? 0 : restOfNumberOfCPF;
+};
+
+console.log(cpfDigit());
